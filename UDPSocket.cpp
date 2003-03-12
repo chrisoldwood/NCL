9375aafe65b5bb34b0854bdf461f65146252e634
Administrator <administrator@chrisoldwood.com>
2003-03-12 12:00:04 +0000

    Changed class to be a base class for concrete client & server UDP sockets. Added SendTo() and RecvFrom() methods.

--- UDPSocket.cpp
+++ UDPSocket.cpp
@@ -46,3 +46,99 @@ CUDPSocket::CUDPSocket()
 CUDPSocket::~CUDPSocket()
 {
 }
+
+/******************************************************************************
+** Methods:		Type() & Protocol()
+**
+** Description:	Template methods to get the socket type and protocol.
+**
+** Parameters:	None.
+**
+** Returns:		SOCK_* & IPPROTO_*.
+**
+*******************************************************************************
+*/
+
+int CUDPSocket::Type() const
+{
+	return SOCK_DGRAM;
+}
+
+int CUDPSocket::Protocol() const
+{
+	return IPPROTO_UDP;
+}
+
+/******************************************************************************
+** Method:		SendTo()
+**
+** Description:	Sends data to a specific host and port.
+**
+** Parameters:	pBuffer		The buffer to send.
+**				nBufSize	The buffer size.
+**				oAddr		The target host.
+**				nPort		The target port number.
+**
+** Returns:		The number of bytes sent.
+**
+** Exceptions:	CSocketException.
+**
+*******************************************************************************
+*/
+
+int CUDPSocket::SendTo(const void* pBuffer, int nBufSize, const in_addr& oAddr, int nPort)
+{
+	ASSERT(m_hSocket != INVALID_SOCKET);
+
+	sockaddr_in	addr = { 0 };
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr   = oAddr;
+	addr.sin_port   = htons((ushort)nPort);
+
+	int nResult = sendto(m_hSocket, (const char*)pBuffer, nBufSize, 0, (sockaddr*)&addr, sizeof(addr));
+
+	if (nResult == SOCKET_ERROR)
+		throw CSocketException(CSocketException::E_SEND_FAILED, CWinSock::LastError());
+
+	ASSERT(nResult == nBufSize);
+
+	return nResult;
+}
+
+/******************************************************************************
+** Method:		RecvFrom()
+**
+** Description:	Read incoming data.
+**
+** Parameters:	pBuffer		The buffer to write to.
+**				nBufSize	The buffer size.
+**				oAddr		The source host.
+**				nPort		The source port number.
+**
+** Returns:		The number of bytes read.
+**
+** Exceptions:	CSocketException.
+**
+*******************************************************************************
+*/
+
+int CUDPSocket::RecvFrom(void* pBuffer, int nBufSize, in_addr& oAddr, int& nPort)
+{
+	ASSERT(m_hSocket != INVALID_SOCKET);
+
+	sockaddr_in	addr;
+	int         nLength;
+
+	int nResult = recvfrom(m_hSocket, (char*)pBuffer, nBufSize, 0, (sockaddr*)&addr, &nLength);
+
+	if (nResult == SOCKET_ERROR)
+		throw CSocketException(CSocketException::E_RECV_FAILED, CWinSock::LastError());
+
+	ASSERT(addr.sin_family == AF_INET);
+
+	oAddr = addr.sin_addr;
+	nPort = ntohs(addr.sin_port);
+
+	return nResult;
+}
--- UDPSocket.hpp
+++ UDPSocket.hpp
@@ -22,16 +22,33 @@
 class CUDPSocket : public CSocket
 {
 public:
-	//
-	// Constructors/Destructor.
-	//
-	CUDPSocket();
 	virtual ~CUDPSocket();
-	
+
+	//
+	// Methods.
+	//
+	int SendTo(const void* pBuffer, int nBufSize, const in_addr& oAddr, int nPort);
+	int SendTo(const CBuffer& oBuffer, const in_addr& oAddr, int nPort);
+	int SendTo(const char* pszString, const in_addr& oAddr, int nPort);
+
+	int RecvFrom(void* pBuffer, int nBufSize, in_addr& oAddr, int& nPort);
+	int RecvFrom(CBuffer& oBuffer, in_addr& oAddr, int& nPort);
+
 protected:
 	//
 	// Members.
 	//
+
+	// Protect creation etc.
+	CUDPSocket();
+	CUDPSocket(const CUDPSocket&);
+	void operator=(const CUDPSocket&);
+
+	//
+	// Template methods.
+	//
+	virtual int Type()     const;
+	virtual int Protocol() const;
 };
 
 /******************************************************************************
@@ -40,5 +57,20 @@ protected:
 **
 *******************************************************************************
 */
+
+inline int CUDPSocket::SendTo(const CBuffer& oBuffer, const in_addr& oAddr, int nPort)
+{
+	return SendTo(oBuffer.Buffer(), oBuffer.Size(), oAddr, nPort);
+}
+
+inline int CUDPSocket::SendTo(const char* pszString, const in_addr& oAddr, int nPort)
+{
+	return SendTo(pszString, strlen(pszString), oAddr, nPort);
+}
+
+inline int CUDPSocket::RecvFrom(CBuffer& oBuffer, in_addr& oAddr, int& nPort)
+{
+	return RecvFrom(oBuffer.Buffer(), oBuffer.Size(), oAddr, nPort);
+}
 
 #endif // UDPSOCKET_HPP
