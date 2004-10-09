646ea934fa75e18a70f90f6d7c4f7f441e728603
Administrator <administrator@chrisoldwood.com>
2004-10-09 09:32:19 +0000

    Generated.

---
+++ NetBuffer.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		NETBUFFER.CPP
+** COMPONENT:	Network & Comms Library
+** DESCRIPTION:	CNetBuffer class definition.
+**
+*******************************************************************************
+*/
+
+#include "ncl.hpp"
+
+#ifdef _DEBUG
+// For memory leak detection.
+#define new DBGCRT_NEW
+#endif
+
+/******************************************************************************
+** Method:		Constructor.
+**
+** Description:	.
+**
+** Parameters:	None.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+CNetBuffer::CNetBuffer()
+{
+}
+
+/******************************************************************************
+** Method:		Destructor.
+**
+** Description:	.
+**
+** Parameters:	None.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+CNetBuffer::~CNetBuffer()
+{
+}
---
+++ NetBuffer.hpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		NETBUFFER.HPP
+** COMPONENT:	Network & Comms Library
+** DESCRIPTION:	The CNetBuffer class declaration.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef NETBUFFER_HPP
+#define NETBUFFER_HPP
+
+/******************************************************************************
+** 
+** A FIFO buffer.
+**
+*******************************************************************************
+*/
+
+class CNetBuffer
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	CNetBuffer();
+	~CNetBuffer();
+	
+protected:
+	//
+	// Members.
+	//
+};
+
+/******************************************************************************
+**
+** Implementation of inline functions.
+**
+*******************************************************************************
+*/
+
+#endif // NETBUFFER_HPP
