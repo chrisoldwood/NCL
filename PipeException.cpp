751078f9e684c16aa6442abc8d5f29786f659df5
Admin <admin@chrisoldwood.com>
2007-07-14 14:00:53 +0000

    Removed redundant ctor that takes an error code. Fixed up calls to App.FormatError() to CStrCvt::FormatError().

--- PipeException.cpp
+++ PipeException.cpp
@@ -9,41 +9,12 @@
 */
 
 #include "ncl.hpp"
+#include <WCL/StrCvt.hpp>
 
 #ifdef _DEBUG
 // For memory leak detection.
 #define new DBGCRT_NEW
 #endif
-
-/******************************************************************************
-** Method:		Constructor.
-**
-** Description:	Formats an exception which does not use GetLastError().
-**
-** Parameters:	eErrCode	The exception code
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
-
-CPipeException::CPipeException(int eErrCode)
-	: CException(eErrCode)
-	, m_hResult(NO_ERROR)
-{
-	// Convert error to string.
-	switch(eErrCode)
-	{
-		case E_BAD_PROTOCOL:
-			m_strErrorText.Format("Invalid pipe protocol version");
-			break;
-
-		// Shouldn't happen!
-		default:
-			ASSERT_FALSE();
-			break;
-	}
-}
 
 /******************************************************************************
 ** Method:		Constructor.
@@ -59,34 +30,37 @@ CPipeException::CPipeException(int eErrCode)
 */
 
 CPipeException::CPipeException(int eErrCode, HRESULT hResult)
-	: CException(eErrCode)
-	, m_hResult(hResult)
+	: m_hResult(hResult)
 {
 	// Convert error to string.
 	switch(eErrCode)
 	{
 		case E_CREATE_FAILED:
-			m_strErrorText.Format("Failed to create pipe:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to create pipe:\n\n%s", CStrCvt::FormatError(hResult));
 			break;
 
 		case E_OPEN_FAILED:
-			m_strErrorText.Format("Failed to open pipe:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to open pipe:\n\n%s", CStrCvt::FormatError(hResult));
 			break;
 
 		case E_ACCEPT_FAILED:
-			m_strErrorText.Format("Failed to accept pipe connection:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to accept pipe connection:\n\n%s", CStrCvt::FormatError(hResult));
 			break;
 
 		case E_PEEK_FAILED:
-			m_strErrorText.Format("Failed to peek from pipe:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to peek from pipe:\n\n%s", CStrCvt::FormatError(hResult));
 			break;
 
 		case E_READ_FAILED:
-			m_strErrorText.Format("Failed to read from pipe:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to read from pipe:\n\n%s", CStrCvt::FormatError(hResult));
 			break;
 
 		case E_WRITE_FAILED:
-			m_strErrorText.Format("Failed to write to pipe:\n\n%s", CApp::This().FormatError(hResult));
+			m_strErrorText.Format("Failed to write to pipe:\n\n%s", CStrCvt::FormatError(hResult));
+			break;
+
+		case E_BAD_PROTOCOL:
+			m_strErrorText.Format("Invalid pipe protocol version");
 			break;
 
 		// Shouldn't happen!
