25a1ebc5ac769b4853428965aa2ab4a444d36807
Admin <admin@chrisoldwood.com>
2007-10-15 20:45:19 +0000

    Initial version.

---
+++ Common.hpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   Common.hpp
+//! \brief  Wrapper to include the most common NCL library headers.
+//! \author Chris Oldwood
+
+// Check for previous inclusion
+#ifndef NCL_COMMON_HPP
+#define NCL_COMMON_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// Library documentation
+//
+//! \namespace NCL
+//! \brief     The Network & Comms Library namespace.
+
+////////////////////////////////////////////////////////////////////////////////
+// Standard headers.
+
+#include <Core/Common.hpp>		// Core library common headers.
+#include <WCL/Common.hpp>		// Windows C++ library common headers.
+
+////////////////////////////////////////////////////////////////////////////////
+// Library headers.
+
+#endif // NCL_COMMON_HPP
