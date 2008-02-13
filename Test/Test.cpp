7efe31e0994fc1047583f7e15fddd864a1135dfb
Admin <admin@chrisoldwood.com>
2008-02-13 08:03:52 +0000

    Initial version.

---
+++ Test/Test.cpp
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+
+#include "stdafx.h"
+#include <tchar.h>
+#include <Core/UnitTest.hpp>
+
+int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+{
+	return Core::GetTestProcessResult();
+}
