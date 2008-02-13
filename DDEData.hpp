07b140e8d6cc3519e53fad649fadb2d8f4c1a874
Admin <admin@chrisoldwood.com>
2008-02-13 13:40:54 +0000

    Moved the majority of the implementation to a separate source file. Modified code to build as both ANSI and Unicode. Fixed up code that used int for sizes and indexes to use size_t.

--- DDEData.hpp
+++ DDEData.hpp
@@ -38,7 +38,7 @@ public:
 	//
 	CDDEData(CDDEInst* pInst, HDDEDATA hData, uint nFormat, bool bOwn);
 	CDDEData(CDDEInst* pInst, HSZ hItem, uint nFormat, bool bOwn);
-	CDDEData(CDDEInst* pInst, const void* pBuffer, uint nSize, uint nOffset, uint nFormat, bool bOwn);
+	CDDEData(CDDEInst* pInst, const void* pBuffer, size_t nSize, size_t nOffset, uint nFormat, bool bOwn);
 	CDDEData(CDDEInst* pInst, const CBuffer& oBuffer, uint nFormat, bool bOwn);
 	CDDEData(const CDDEData& oData);
 	~CDDEData();
@@ -52,17 +52,17 @@ public:
 	// Accessors.
 	//
 	HDDEDATA Handle() const;
-	uint     Size() const;
-	uint     GetData(void* pBuffer, uint nSize, uint nOffset = 0) const;
+	size_t   Size() const;
+	size_t   GetData(void* pBuffer, size_t nSize, size_t nOffset = 0) const;
 	CBuffer  GetBuffer() const;
-	CString  GetString() const;
+	CString  GetString(TextFormat eFormat) const;
 
 	//
 	// Mutators.
 	//
-	void SetData(const void* pBuffer, uint nSize, uint nOffset = 0);
+	void SetData(const void* pBuffer, size_t nSize, size_t nOffset = 0);
 	void SetBuffer(const CBuffer& oBuffer);
-	void SetString(const char* pszString);
+	void SetAnsiString(const CString& str, TextFormat eFormat);
 
 	void Free();
 
@@ -106,98 +106,6 @@ public:
 *******************************************************************************
 */
 
-inline CDDEData::CHandle::CHandle(CDDEInst* pInst, HDDEDATA hData, uint nFormat, bool bOwn)
-	: m_nRefCount(1)
-	, m_pInst(pInst)
-	, m_hData(hData)
-	, m_nFormat(nFormat)
-	, m_bOwn(bOwn)
-{
-	ASSERT(pInst != NULL);
-}
-
-inline CDDEData::CHandle::~CHandle()
-{
-	ASSERT(m_nRefCount == 0);
-
-	// Free handle?
-	if (m_bOwn)
-		::DdeFreeDataHandle(m_hData);
-}
-
-inline CDDEData::CDDEData(CDDEInst* pInst, HDDEDATA hData, uint nFormat, bool bOwn)
-	: m_pHandle(new CHandle(pInst, hData, nFormat, bOwn))
-{
-}
-
-inline CDDEData::CDDEData(CDDEInst* pInst, HSZ hItem, uint nFormat, bool bOwn)
-{
-	// Allocate data handle.
-	HDDEDATA hData = ::DdeCreateDataHandle(pInst->Handle(), NULL, 0, 0, hItem, nFormat, 0);
-
-	if (hData == NULL)
-		throw CDDEException(CDDEException::E_ALLOC_FAILED, pInst->LastError());
-
-	// Attach handle.
-	m_pHandle = new CHandle(pInst, hData, nFormat, bOwn);
-}
-
-inline CDDEData::CDDEData(CDDEInst* pInst, const void* pBuffer, uint nSize, uint nOffset, uint nFormat, bool bOwn)
-{
-	// Allocate data handle.
-	HDDEDATA hData = ::DdeCreateDataHandle(pInst->Handle(), (byte*)pBuffer, nSize, nOffset, NULL, nFormat, 0);
-
-	if (hData == NULL)
-		throw CDDEException(CDDEException::E_ALLOC_FAILED, pInst->LastError());
-
-	// Attach handle.
-	m_pHandle = new CHandle(pInst, hData, nFormat, bOwn);
-}
-
-inline CDDEData::CDDEData(CDDEInst* pInst, const CBuffer& oBuffer, uint nFormat, bool bOwn)
-{
-	// Allocate data handle.
-	HDDEDATA hData = ::DdeCreateDataHandle(pInst->Handle(), (byte*)oBuffer.Buffer(), oBuffer.Size(), 0, NULL, nFormat, 0);
-
-	if (hData == NULL)
-		throw CDDEException(CDDEException::E_ALLOC_FAILED, pInst->LastError());
-
-	// Attach handle.
-	m_pHandle = new CHandle(pInst, hData, nFormat, bOwn);
-}
-
-inline CDDEData::CDDEData(const CDDEData& oData)
-	: m_pHandle(oData.m_pHandle)
-{
-	ASSERT(m_pHandle != NULL);
-
-	++(m_pHandle->m_nRefCount);
-}
-
-inline CDDEData::~CDDEData()
-{
-	ASSERT(m_pHandle != NULL);
-
-	if (--(m_pHandle->m_nRefCount) == 0)
-		delete m_pHandle;
-}
-
-inline CDDEData& CDDEData::operator=(const CDDEData& oData)
-{
-	ASSERT(this != &oData);
-	ASSERT(m_pHandle != NULL);
-	ASSERT(oData.m_pHandle != NULL);
-
-	if (--(m_pHandle->m_nRefCount) == 0)
-		delete m_pHandle;
-	
-	m_pHandle = oData.m_pHandle;
-
-	++(m_pHandle->m_nRefCount);
-
-	return *this;
-}
-
 inline HDDEDATA CDDEData::Handle() const
 {
 	ASSERT(m_pHandle != NULL);
@@ -205,85 +113,19 @@ inline HDDEDATA CDDEData::Handle() const
 	return m_pHandle->m_hData;
 }
 
-inline uint CDDEData::Size() const
-{
-	ASSERT(m_pHandle != NULL);
-
-	if (m_pHandle->m_hData == NULL)
-		return 0;
-
-	return ::DdeGetData(m_pHandle->m_hData, NULL, 0, 0);
-}
-
-inline uint CDDEData::GetData(void* pBuffer, uint nSize, uint nOffset) const
+inline size_t CDDEData::GetData(void* pBuffer, size_t nSize, size_t nOffset) const
 {
 	ASSERT(m_pHandle != NULL);
 	ASSERT(m_pHandle->m_hData != NULL);
 	ASSERT(pBuffer != NULL);
 	ASSERT(nOffset+nSize <= Size());
 
-	return ::DdeGetData(m_pHandle->m_hData, (byte*)pBuffer, nSize, nOffset);
-}
-
-inline CBuffer CDDEData::GetBuffer() const
-{
-	uint nSize = Size();
-
-	CBuffer oBuffer(nSize);
-
-	GetData(oBuffer.Buffer(), nSize, 0);
-
-	return oBuffer;
-}
-
-inline CString CDDEData::GetString() const
-{
-	uint nSize = Size();
-
-	// Allocate a temporary buffer.
-	char* psz = (char*) _alloca(nSize+1);
-
-	// Get the data contents.
-	GetData((byte*)psz, nSize, 0);
-
-	// Ensure its null terminated.
-	psz[nSize] = '\0';
-
-	return CString(psz);
-}
-
-inline void CDDEData::SetData(const void* pBuffer, uint nSize, uint nOffset)
-{
-	ASSERT(m_pHandle != NULL);
-	ASSERT((pBuffer != NULL) || (nSize == 0));
-
-	HDDEDATA hData = ::DdeAddData(m_pHandle->m_hData, (byte*)pBuffer, nSize, nOffset);
-
-	if (hData == NULL)
-		throw CDDEException(CDDEException::E_ALLOC_FAILED, m_pHandle->m_pInst->LastError());
-
-	m_pHandle->m_hData = hData;
+	return ::DdeGetData(m_pHandle->m_hData, static_cast<byte*>(pBuffer), nSize, nOffset);
 }
 
 inline void CDDEData::SetBuffer(const CBuffer& oBuffer)
 {
 	SetData(oBuffer.Buffer(), oBuffer.Size());
-}
-
-inline void CDDEData::SetString(const char* pszString)
-{
-	// NB: Must include null terminator.
-	SetData(pszString, strlen(pszString)+1);
-}
-
-inline void CDDEData::Free()
-{
-	ASSERT(m_pHandle != NULL);
-
-	if (m_pHandle->m_hData != NULL)
-		::DdeFreeDataHandle(m_pHandle->m_hData);
-
-	m_pHandle->m_hData = NULL;
 }
 
 #endif // DDEDATA_HPP
