50dd2d9f6e856498159e3247f61ae5c2ccf42019
Admin <admin@chrisoldwood.com>
2008-03-30 20:49:25 +0000

    Changed to use the new TEST_SUITE macros.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,11 +1,23 @@
 ////////////////////////////////////////////////////////////////////////////////
-//
+//! \file   Test.cpp
+//! \brief  The test harness entry point.
+//! \author Chris Oldwood
 
 #include "stdafx.h"
 #include <tchar.h>
 #include <Core/UnitTest.hpp>
 
+////////////////////////////////////////////////////////////////////////////////
+// The test group functions.
+
+////////////////////////////////////////////////////////////////////////////////
+//! The entry point for the test harness.
+
 int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
 {
-	return Core::GetTestProcessResult();
+	TEST_SUITE_BEGIN
+	{
+		Core::SetTestRunFinalStatus(true);
+	}
+	TEST_SUITE_END
 }
